8cec882c52f2009c2c5d48114bf91bf228983531
Stepan Vasilyev <intellectualspecialist@gmail.com>
2025-11-15 19:35:45 +0500

    fix: вносит правки по критериям

---
+++ scripts/modal.js
@@ -0,0 +1,78 @@
+import { isEscapeKey } from './utils.js';
+
+const bodyElement = document.body;
+const containerElement = bodyElement.querySelector('.container');
+const openButtonElement = containerElement.querySelector('.js-modal-button');
+const modalElement = bodyElement.querySelector('.modal');
+let closeButtonElement;
+let checkInInputElement;
+
+if (modalElement) {
+  closeButtonElement = modalElement.querySelector('.modal__close');
+  checkInInputElement = modalElement.querySelector('[name="check-in"]');
+}
+
+const loopFocusOnModal = () => {
+  containerElement.setAttribute('inert', true);
+};
+
+const removeLoopFocusOnModal = () => {
+  containerElement.removeAttribute('inert');
+};
+
+const onButtonClick = (evt) => {
+  evt.preventDefault();
+  openModal();
+};
+
+const onCloseButtonClick = () => {
+  closeModal();
+};
+
+const onOverlayClick = (evt) => {
+  if (evt.target.closest('.modal__content')) {
+    evt.stopPropagation();
+  } else {
+    closeModal();
+  }
+};
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const registerButtonClickEvents = () => {
+  openButtonElement.addEventListener('click', onButtonClick);
+};
+
+function openModal() {
+  modalElement.classList.remove('modal--hidden');
+  closeButtonElement.addEventListener('click', onCloseButtonClick);
+  setTimeout(() => {
+    document.addEventListener('click', onOverlayClick);
+  }, 0);
+  document.addEventListener('keydown', onDocumentKeydown);
+  loopFocusOnModal();
+  checkInInputElement.focus();
+  bodyElement.style = 'overflow: hidden;';
+}
+
+function closeModal() {
+  modalElement.classList.add('modal--hidden');
+  closeButtonElement.removeEventListener('click', onCloseButtonClick);
+  document.removeEventListener('click', onOverlayClick);
+  document.removeEventListener('keydown', onDocumentKeydown);
+  removeLoopFocusOnModal();
+  bodyElement.removeAttribute('style');
+}
+
+const initModal = () => {
+  if (openButtonElement) {
+    registerButtonClickEvents();
+  }
+};
+
+export { initModal };
